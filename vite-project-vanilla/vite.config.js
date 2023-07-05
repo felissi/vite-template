3c89ab3bb93b4dccd911f68b22e0ff2b2dc7619b
Felix <hkecgl-felixwong@hkecl.net>
2023-07-05 10:09:40 +0800

    init

---
+++ vite-project-vanilla/vite.config.js
@@ -0,0 +1,12 @@
+import path from "path";
+
+export default {
+  // ...
+  resolve: {
+    alias: {
+      "@": ".",
+      "@assets": "./assets",
+      "@features": "./features",
+    },
+  },
+};
